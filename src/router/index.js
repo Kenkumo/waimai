4e8490982915d940589bffe95d1e06f64aeb7fd3
xiaosu <sujianyun2019@163.com>
2020-06-01 22:32:54 +0800

    基础配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Msite from '../views/msite/Msite.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: Msite
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/msite',
+    name: 'Msite',
+    component: () => import('../views/msite/Msite.vue')
+  },
+  
 ]
 
 const router = new VueRouter({
